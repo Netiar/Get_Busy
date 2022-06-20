84a74f0b034ae920b3ddee20fd9fd00d3c380ae1
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-20 17:28:29 +0300

    import the android R class

--- app/src/main/java/com/moringaschool/get_busy/ui/RegisterActivity.java
+++ app/src/main/java/com/moringaschool/get_busy/ui/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.moringaschool.get_busy.R;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
