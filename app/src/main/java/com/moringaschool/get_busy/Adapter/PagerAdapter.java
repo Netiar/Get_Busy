cc655fe15e09b642ee616ad42ee6b69f3ccb16fe
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-22 10:04:42 +0300

    bind questions data to the fragment

--- app/src/main/java/com/moringaschool/get_busy/Adapter/PagerAdapter.java
+++ app/src/main/java/com/moringaschool/get_busy/Adapter/PagerAdapter.java
@@ -1,2 +1,40 @@
-package com.moringaschool.get_busy.Adapter;public class PagerAdapter {
+package com.moringaschool.get_busy.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.moringaschool.get_busy.fragments.QuestFragment;
+import com.moringaschool.get_busy.models.Result__1;
+
+import java.util.List;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    List<Result__1> allQuests;
+
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior, List<Result__1> allQuest) {
+        super(fm, behavior);
+
+        this.allQuests = allQuest;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        QuestFragment questFragment = QuestFragment.newInstance(allQuests.get(position));
+        return questFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return allQuests.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return allQuests.get(position).getCategory();
+    }
 }
