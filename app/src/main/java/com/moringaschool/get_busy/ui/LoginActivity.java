785f501c5a22b1f6712a61de90ce7f7a4bf0ed54
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-23 12:31:23 +0300

    initialize a firebase app to allow unit testing in the activity.

--- app/src/main/java/com/moringaschool/get_busy/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/get_busy/ui/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,6 +32,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         logBind = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(logBind.getRoot());
+
+        FirebaseApp.initializeApp(this);
 
         myAuth = FirebaseAuth.getInstance();
         createAuthListener();
