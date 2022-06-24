654e2e2c9e213eb100245da63e46407554a0d183
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-24 03:47:15 +0300

    remove unwanted views from the layout

--- app/src/main/java/com/moringaschool/get_busy/ui/EducationalActivity.java
+++ app/src/main/java/com/moringaschool/get_busy/ui/EducationalActivity.java
@@ -34,8 +34,9 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
     RecyclerViewAdapter adapter;
     ActivityEducationalBinding binding;
     int userScore;
-    Call<ResultOpenDb> call1, call2, call3, call4, call5, call6;
+    Call<ResultOpenDb> call1;
     public List<Result__1> allItemsList;
+    Result__1 question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,25 +46,19 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
 
         allItemsList=new ArrayList<>();
 
+        Intent intent = getIntent();
+        int position = intent.getIntExtra("position", 0);
+        question = (Result__1) intent.getSerializableExtra("result");
+        int num =  10 + position;
+
+        binding.submit.setOnClickListener(this);
+
+
         EducationalApi client = EducationalApiClient.getClient();
         //https://opentdb.com/api.php?amount=50&category=18&difficulty=medium&type=multiple
-         call1 = client.getAllItems(5,18,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
-         call2 = client.getAllItems(5,10,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
-         call3 = client.getAllItems(5,12,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
-         call4 = client.getAllItems(5,16,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
-         call5 = client.getAllItems(5,17,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
-         call6 = client.getAllItems(5,19,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
+         call1 = client.getAllItems(5,num,"medium","multiple"); //amount=1&category=27&difficulty=easy&type=multiple
 
-        binding.cat1.setOnClickListener(this);
-        binding.cat2.setOnClickListener(this);
-        binding.cat3.setOnClickListener(this);
-        binding.cat4.setOnClickListener(this);
-        binding.cat5.setOnClickListener(this);
-        binding.submit.setOnClickListener(this);
-        binding.submit.setOnClickListener(this);
-        setNavigation(binding.bottom);
-
-        getResponse(call1);
+       getResponse(call1);
     }
 
     private void getResponse(Call<ResultOpenDb> call) {
@@ -84,6 +79,7 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
                     binding.lvp.setHasFixedSize(true);
                     successful();
                 }else{
+
                     unSuccessful();
                 }
             }
@@ -96,27 +92,12 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        if (v == binding.cat1){
-            getResponse(call6);
-            return;
-        }else if (v == binding.cat2){
-            getResponse(call2);
-            return;
-        }else if (v == binding.cat3){
-            getResponse(call3);
-            return;
-        }else if (v == binding.cat4){
-            getResponse(call4);
-            return;
-        }else if (v == binding.cat5){
-            getResponse(call5);
-            return;
-        } else if(v == binding.cat6){
-            startActivity(new Intent(this, UserQuestionsActivity.class));
-        }
-        else if (v == binding.submit){
+        if (v == binding.submit){
             userScore = RecyclerViewAdapter.getScore();
-            Toast.makeText(this, "Your Score is " + userScore, Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(EducationalActivity.this, AnsActivity.class);
+            intent.putExtra("userScore", userScore);
+            startActivity(intent);
+//            Toast.makeText(this, "Your Score is " + userScore, Toast.LENGTH_LONG).show();
         }
         else{
             Toast.makeText(this, "Keep calm", Toast.LENGTH_SHORT).show();
@@ -124,8 +105,6 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
     }
 
     public void successful(){
-        binding.bottom.setVisibility(View.VISIBLE);
-        binding.scroll.setVisibility(View.VISIBLE);
         binding.bored.setVisibility(View.VISIBLE);
         binding.welcome.setVisibility(View.VISIBLE);
         binding.lvp.setVisibility(View.VISIBLE);
@@ -139,36 +118,8 @@ public class EducationalActivity extends AppCompatActivity implements View.OnCli
         binding.lvp.setVisibility(View.GONE);
         binding.submit.setVisibility(View.GONE);
         binding.progress.setVisibility(View.GONE);
-        binding.bottom.setVisibility(View.GONE);
 
 
     }
 
-    public void setNavigation(BottomNavigationView bottom){
-        bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                        startActivity(new Intent(EducationalActivity.this, MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.education:
-                        return true;
-                    case R.id.favourites:
-                        startActivity(new Intent(EducationalActivity.this, UserQuestionsActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.back:
-                        Intent intent = new Intent(EducationalActivity.this, LoginActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(intent);
-                        finish();
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
-            }
-        });
-    }
 }
