f766145bad74a54f0481ffdc1c8faef84cd1f70c
name <collins.odinga@student.moringaschool.com>
2022-06-24 04:32:44 +0300

    rf: refactored code

--- app/src/main/java/com/moringaschool/get_busy/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/get_busy/ui/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.back:
-                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        Intent intent = new Intent(MainActivity.this, SplashActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                         finish();
